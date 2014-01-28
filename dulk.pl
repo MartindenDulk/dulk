c967c4284c82f02507a85169d972c93312a1d639
MartindenDulk <martin@dendulk.org>
2014-01-28 22:59:58 +0100

    Moved code, added plugin demo
    - Created basics for plugin loading / message handling
    - Categorized subroutines and moved some to another module
    - Editted the way incoming messages are assigned to scalars

--- dulk.pl
+++ dulk.pl
@@ -3,15 +3,17 @@
 # A simple IRC robot.
 # Usage: perl irc.pl
 
-
 # define our own lib dir
 use FindBin qw($RealBin);
 use lib "$RealBin/lib/";
+use lib "$RealBin/lib/dulk/plugin";
 
 # Add some Modules we need
 use strict;
 use Data::Dumper;
 use dulk::Base; # Handles socket connections / Errors / ...
+
+$|++;
 
 # Global variable 
 my $bot = new dulk::Base;
@@ -19,7 +21,4 @@ my $bot = new dulk::Base;
 # Connect and check status if it´s connected
 my $status = $bot->connect();
 
-if ($bot->getStatus() eq "connected") {
-    print "We are connected, start listening for messages.";
-}
 
--- lib/dulk/Base.pm
+++ lib/dulk/Base.pm
@@ -1,41 +1,69 @@
 package dulk::Base;
 
-#constructor
-sub new {
-  my $self = {};
-  bless $self, 'dulk::Base';
-  return $self;
-}
+    # global variables
+    my $bot;
+
+    #constructor
+    sub new {
+      my $self = {};
+      bless $self, 'dulk::Base';
+      return $self;
+    }
 
 
-# Socket connect.
-sub connect {
-    require dulk::Socket;
-    my $bot = new dulk::Socket;
-    $socket = $bot->createSocket();
-    setStatus($socket);
-}
+    # Socket connect.
+    sub connect {
+        require dulk::Socket;
+        $bot = new dulk::Socket;
+        $socket = $bot->createSocket();
+        setStatus($socket);
+    }
 
-# Error handling. Just a print to console. We could make this configurable. Perhaps relayed to the debug channel?
-sub throwError {
-    (my $errorMessage, $errorScript) = @_;
-    print "[ERROR - $errorScript] $errorMessage\n";
-}
+    # Error handling. Just a print to console. We could make this configurable. Perhaps relayed to the debug channel?
+    sub throwError {
+        (my $errorMessage, $errorScript) = @_;
+        print "[ERROR - $errorScript] $errorMessage\n";
+    }
 
-# Status handling. Set & Get.
-my $status = "";
-sub setStatus {
-    (my $statusMessage) = @_;
-    if (defined $statusMessage) { $status = $statusMessage."\n"; }
-    else { throwError("Tried to set the status with an undefined message",__PACKAGE__); }
-}
+    # Status handling. Set & Get.
+    my $status = "";
+    sub setStatus {
+        (my $statusMessage) = @_;
+        if (defined $statusMessage) { $status = $statusMessage."\n"; }
+        else { throwError("Tried to set the status with an undefined message",__PACKAGE__); }
+    }
 
-sub getStatus {
-    if (defined $status) { return $status; }
-    else { return "Status unknown."; }
-}
+    sub getStatus {
+        if (defined $status) { return $status; }
+        else { return "Status unknown."; }
+    }
 
+    sub loadPlugins {
+        my $dir = "lib/dulk/plugin";
+        opendir (DIR, $dir) or throwError("Error opening plugin folder:$!",__PACKAGE__);
 
+        while (my $file = readdir(DIR)) {
+          if ($file =~ m/(.*?)(?:\.pm|\.pl)/gi) {
+          my $newModule = $1;
+            require "$file";
+              push(@plugins, "dulk::Plugin::$newModule");
+          }
+        }
+        closedir(DIR);
+    }
+
+    sub messageReceived {
+      my @message = @_;
+
+      foreach(@plugins) {
+        $_->public(@message);
+      }
+
+    }
+
+    sub relayMessage {
+      $bot->relayMessage("test");
+    }
 1;
 
 
