b307e04dd7c9688c2505d0e9d1411975f5d024d6
MartindenDulk <martin@dendulk.org>
2014-01-29 20:52:25 +0100

    Finished plugin template
    Has a working example. Read the comments in the file to see what it's
    capable of.

--- lib/dulk/plugin/Foo.pm
+++ lib/dulk/plugin/Foo.pm
@@ -14,16 +14,18 @@ my $bot = new dulk::Base;
 
 sub public {
 
-  my @query = @_[ 2 .. $#_ ];
-    my ($raw, $nickname, $message, $destination, $type) = @query;
+  my @input = @_[ 2 .. $#_ ];
+    my ($raw, $nickname, $message, $destination, $type) = @input;
 
-    if ($message eq 'foo') {
-        $bot->relayMessage("Foo.pm: Foo","#mojitotest");
+    ### @query now holds your message splitted. You can check for input by checking the values of $query[x]
+    my @query = split(' ',$message);
+
+    if ($query[0] eq 'foo') {
+      ### If the word 'foo' is said on a channel where the bot is. It will relay a message to that channel
+      $bot->relayMessage("Hey! I just received $query[0] from $nickname on $destination. The full command was $message",$destination);
     }
 
-    if ($message eq 'martin') {
-        $bot->relayMessage("Koffie? :D","#mojitotest");
-    }
+
 
 }
 
