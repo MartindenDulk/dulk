7605f2129a0e4e1a6ae6feab9aa36c4f987f0942
MartindenDulk <martin@dendulk.org>
2014-01-31 19:07:24 +0100

    Document formatting / added header

--- lib/dulk/plugin/Foo.pm
+++ lib/dulk/plugin/Foo.pm
@@ -1,32 +1,58 @@
-package dulk::plugin::Foo;
+#########################################################
+### 
+### File: Foo.pm
+### Author: Martin den Dulk
+### Contact: martin@dendulk.org
+### 
+### ======
+### 
+### This file was created for the dulk IRC bot repository
+### on GitHub. See: https://github.com/MartindenDulk/dulk 
+### 
+#########################################################
 
-$|++;
+  package dulk::plugin::Foo;
 
-#constructor
-sub new {
-  my $self = {};
-  bless $self, 'dulk::plugin::Foo';
-  return $self;
-}
+  $|++;
 
-use dulk::Base;
-my $bot = new dulk::Base;
+#########################################################
+### USED MODULES
+#########################################################
 
-sub public {
+  use dulk::Base;
 
-  my @input = @_[ 2 .. $#_ ];
-    my ($raw, $nickname, $message, $destination, $type) = @input;
+#########################################################
+### GLOBAL VARIABLES
+#########################################################
 
-    ### @query now holds your message splitted. You can check for input by checking the values of $query[x]
-    my @query = split(' ',$message);
+  my $bot = new dulk::Base;
 
-    if ($query[0] eq 'foo') {
-      ### If the word 'foo' is said on a channel where the bot is. It will relay a message to that channel
-      $bot->relayMessage("Hey! I just received $query[0] from $nickname on $destination. The full command was $message",$destination);
-    }
+#########################################################
+### SUBROUTINES
+#########################################################
+
+  sub new {
+    my $self = {};
+    bless $self, 'dulk::plugin::Foo';
+    return $self;
+  }
+
+  ### This subroutine is called when the socket receives a message. Include a 'public' subroutine in your own scripts, save it in the plugin folder and it will be called after a rehash.
+  sub public {
+    my @input = @_[ 2 .. $#_ ];
+      ### Strip incoming parameters, assign them to pretty variable names for your comfort
+      my ($raw, $nickname, $message, $destination, $type) = @input;
+
+      ### @query now holds your message splitted. You can check for input by checking the values of $query[x]
+      my @query = split(' ',$message);
 
 
+      ### Dummy command. Boot the bot and say 'foo' in the channel he's on. It will show the response stated below.
+      if ($query[0] eq 'foo') {
 
-}
+        ### Yep, 'foo' was said. Let's talk back!
+        $bot->relayMessage("Hey! I just received $query[0] from $nickname on $destination. The full command was $message",$destination);
+      }
+  }
 
 1;
